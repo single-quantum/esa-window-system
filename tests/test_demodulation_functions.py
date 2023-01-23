915f7d6036424047f1f2edeb77f009a9fc3b7e40
hvlot <h.vlot@singlequantum.com>
2023-01-23 09:54:24 +0100

    updated the tests according to new ppm symbol parse function

--- tests/test_demodulation_functions.py
+++ tests/test_demodulation_functions.py
@@ -7,12 +7,13 @@ from parse_ppm_symbols import parse_ppm_symbols_new
 
 def test_parse_ppm_symbols_single_noise_symbol():
     # With a pulse falling exactly on the slot edge, it should be parsed as a darkcount.
-    symbols, _ = parse_ppm_symbols_new(np.array([0.5E-6]), 0.1E-6, 1E-6)
-    assert len(symbols) == 0
+    symbols = parse_ppm_symbols_new(np.array([0.1E-6]), 0.1E-6, 1E-6)
+    assert len(symbols) == 1
+    assert symbols[0] == 0
 
 
 def test_parse_ppm_symbols_single_symbol():
-    symbols, _ = parse_ppm_symbols_new(np.array([0.55E-6]), 0.1E-6, 1E-6)
+    symbols = parse_ppm_symbols_new(np.array([0.55E-6]), 0.1E-6, 1E-6)
     assert len(symbols) == 1
     # Approx is needed due to floating point representation
     assert symbols[0] == pytest.approx(5)
@@ -22,8 +23,8 @@ def test_parse_ppm_symbols_multiple_symbols():
     symbol_length = 1E-6
     slot_length = 0.1 * symbol_length
 
-    timestamps = np.arange(0, 100E-6, 1E-6) + 0.5 * slot_length
-    symbols, _ = parse_ppm_symbols_new(timestamps, slot_length, symbol_length)
+    timestamps = np.linspace(0, 99E-6, 100) + 0.5 * slot_length
+    symbols = parse_ppm_symbols_new(timestamps, slot_length, symbol_length)
 
     assert len(symbols) == len(timestamps)
 
@@ -38,6 +39,6 @@ def test_parse_ppm_symbols_multiple_symbols_with_jitter():
     rng = default_rng(777)
     timestamps += rng.normal(0, 0.1 * slot_length, len(timestamps))
 
-    symbols, _ = parse_ppm_symbols_new(timestamps, slot_length, symbol_length)
+    symbols = parse_ppm_symbols_new(timestamps, slot_length, symbol_length)
 
     assert len(symbols) == pytest.approx(0.998 * len(timestamps), rel=1E-3)
