0b76d0b5a4b34839793bebdfb2d3d11b8aa10a19
Andreas <andreas@singlequantum.com>
2023-07-21 10:32:54 +0200

    support acquiring counts from multiple channels

--- acquire_from_time_tagger.py
+++ acquire_from_time_tagger.py
@@ -6,11 +6,17 @@ import TimeTagger
 
 from ppm_parameters import CODE_RATE, M, num_samples_per_slot
 
+num_channels = 4
+# channels = [i+1 for i in range(num_channels)]
+channels = [1, 2, 3, 4]
+
 tagger = TimeTagger.createTimeTagger(resolution=TimeTagger.Resolution.Standard)
-tagger.setDeadtime(1, 2)
 serial = tagger.getSerial()
 print(f'Connected to time tagger {serial}')
-tagger.setTriggerLevel(1, 0.10)
+
+for i in channels:
+    tagger.setDeadtime(i, 10000)
+    tagger.setTriggerLevel(i, 0.10)
 tagger.sync()
 
 sleep(2)
@@ -19,7 +25,7 @@ db_att = 0
 current_time = datetime.now()
 print('Current time: ', current_time)
 formatted_time = current_time.strftime("%H-%M-%S")
-window_size_secs = 50E-3
+window_size_secs = 20E-3
 window_size_ps = window_size_secs * 1E12  # Window time in ps
 
 cr = str(CODE_RATE).replace('/', '-')
@@ -27,11 +33,11 @@ cr = str(CODE_RATE).replace('/', '-')
 filewriter = TimeTagger.FileWriter(
     tagger,
     f'time tagger files/jupiter_tiny_greyscale_{num_samples_per_slot}-sps_{M}-PPM_{cr}-code-rate_{formatted_time}',
-    channels=[1])
+    channels=channels)
 filewriter.startFor(int(window_size_ps), clear=True)
 filewriter.waitUntilFinished()
 
 num_events = filewriter.getTotalEvents()
 
 print(f'{num_events} events written to disk. ')
-print(f'Events per second: {num_events*1/window_size_secs}')
+print(f'Events per second: {num_events*1/window_size_secs:.3e}')
