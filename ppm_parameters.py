f8658800350ee57c8abd0f06110a2cdb6942808c
Henri <h.vlot@singlequantum.com>
2023-08-18 14:43:46 +0200

    added missing type hints

--- ppm_parameters.py
+++ ppm_parameters.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
 
+import numpy.typing as npt
 from numpy import log2
 from PIL import Image
 
@@ -16,7 +17,16 @@ img = Image.open(IMG_FILE_PATH)
 IMG_SHAPE: tuple[int, int] = (img.size[1], img.size[0])
 GREYSCALE: bool = True
 
-# No neEd to change the parameters below, unless you know what you're doing!
+# To calculate where the codewords are, a correlation is made with the CSM. The threshold,
+# as percentage of the maximum correlation of the received sequence, 'decides' whether a peak
+# belongs to the start of a codeword or not
+CORRELATION_THRESHOLD: float = 0.6
+DEBUG_MODE: bool = True
+
+# Usually we record multiple copies of the same message. You can use this to select a particular copy of the message, as sometimes a certain copy of the message has too much noise to recover.
+MESSAGE_IDX: list[int] = [0, 1]
+
+# No need to change the parameters below, unless you know what you're doing!
 # Note: The fraction type is needed for proper match casing
 DAC_DATA_RATE: float = 8.82091E9        # DAC data rate in Hz
 
@@ -27,22 +37,22 @@ slot_factor: float = 5 / 4  # 1 means: no guard slot, 5/4 means: M/4 guard slots
 
 # Channel interleaver parameters
 # The length of each shift register is B*N, with N going from 0 to N-1
-CHANNEL_INTERLEAVE = True
-N_interleaver = 2               # The number of parallel shift registers
+CHANNEL_INTERLEAVE: bool = True
+N_interleaver: int = 2               # The number of parallel shift registers
 
 num_symbols_per_slice: int = 15120
 B_interleaver = int((num_symbols_per_slice/m)/2)            # The base length of each shift register
-symbols_per_codeword = num_symbols_per_slice // m
+symbols_per_codeword: int = num_symbols_per_slice // m
 
 if CHANNEL_INTERLEAVE:
     if not (B_interleaver * N_interleaver) % symbols_per_codeword == 0:
         raise ValueError("The product of B and N should be a multiple of 15120/m")
 
-BIT_INTERLEAVE = True
+BIT_INTERLEAVE: bool = True
 
 num_slots_per_symbol: int = int(slot_factor * M)
 
-CSM = get_csm(M)
+CSM: npt.NDArray = get_csm(M)
 
 sample_size_awg: float = 1 / DAC_DATA_RATE * 1E12       # Time duration of 1 DAC sample in ps
 slot_length: float = sample_size_awg * 1E-12 * num_samples_per_slot  # Length of 1 bin in time
