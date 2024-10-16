06b7b349267b2ecc0078681d018171cdfb64fef9
Henri Vlot <h.vlot@singlequantum.com>
2024-10-16 10:18:46 +0200

    Add test for slicer / puncturing and number of ppm symbols

--- esawindowsystem/tests/integration/test_encoder_functions.py
+++ esawindowsystem/tests/integration/test_encoder_functions.py
@@ -1,7 +1,11 @@
+from fractions import Fraction
+from math import ceil
+
 import numpy as np
 
 from esawindowsystem.core.encoder_functions import (bit_deinterleave, bit_interleave,
                                                     channel_deinterleave, channel_interleave)
+from esawindowsystem.core.scppm_encoder import encoder
 from esawindowsystem.ppm_parameters import B_interleaver, M, N_interleaver
 
 
@@ -49,3 +53,52 @@ def test_bit_interleaving():
     deinterleaved_bits = bit_deinterleave(interleaved_bits)
 
     assert np.all(deinterleaved_bits == input_bits)
+
+
+def test_encoder_num_output_symbols():
+    M = 8
+
+    num_input_bits = 76000
+    input_bit_array = np.ones(num_input_bits, dtype=int)
+    code_rate = Fraction(2, 3)
+
+    m = np.log2(M)
+    N = 2           # Number of parallel shift registers in channel interleaver
+    B = 15120/m/N   # Base length of the shift registers
+
+    # This calculation can be done by hand, following the protocol step by step.
+    information_block_size = 15120*code_rate                                    # 10080 for 2/3 code rate
+    information_block_size_no_termination_bits = information_block_size - 2
+
+    num_information_blocks = ceil(num_input_bits/information_block_size_no_termination_bits)    # 8 information blocks
+    num_bits_into_convolutional_encoder = num_information_blocks*information_block_size
+
+    # For each bit going in, three bits come out and afterwards the codewords are punctured
+    num_bits_after_convolution = num_bits_into_convolutional_encoder*3
+
+    # For 2/3 code rate, half the bits are punctured.
+    num_bits_after_puncturing = num_bits_after_convolution/(3*code_rate)
+    message_ppm_symbols = num_bits_after_puncturing/m
+
+    # As specified by the protocol. This is because the channel interleaver needs
+    # extra steps to make sure the last symbol comes out.
+    num_ppm_symbols_after_interleaving = message_ppm_symbols + B*N*(N-1)
+    num_ppm_codewords = num_ppm_symbols_after_interleaving / (15120 / m)
+
+    # For 8 ppm, the CSM is 16 symbols
+    num_ppm_symbols_with_csm = num_ppm_symbols_after_interleaving + num_ppm_codewords*16
+
+    expected_number_of_symbols = num_ppm_symbols_with_csm
+
+    slot_mapped_sequence, _, information_blocks = encoder(
+        input_bit_array, M, code_rate,
+        **{'B_interleaver': B,
+           'N_interleaver': N}
+    )
+
+    assert information_blocks.shape[0] == num_information_blocks
+    assert information_blocks.shape[1] == information_block_size
+
+    assert slot_mapped_sequence.shape[0] == expected_number_of_symbols
+    # Guard slots should be included at this point
+    assert slot_mapped_sequence.shape[1] == int(5/4*M)
